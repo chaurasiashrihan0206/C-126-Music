2b257983347222908a66193a30ba0c69ca7bae30
Shrihan Chaurasia <78851287+chaurasiashrihan0206@users.noreply.github.com>
2021-11-19 15:48:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song= "";
 
+leftwristX=0;
+leftwristY=0;
+rightwristX=0;
+rightwristY=0;
 function preload(){
     song= loadSound("music.mp3")
 }
@@ -10,6 +14,10 @@ function setup(){
 
     video= createCapture(VIDEO);
     video.hide();
+
+    poseNet= ml5.poseNet(video , modelLoaded);
+
+    poseNet.on('pose' , gotPoses);
 }
 
 function draw(){
@@ -18,4 +26,23 @@ function draw(){
 
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function modelLoaded(){
+    console.log('poseNet is initialized');
+}
+
+function gotPoses(results){
+if(results.length>0){
+    console.log(results);
+leftwristX=results[0].pose.leftWrist.x;
+leftwristY=results[0].pose.leftWrist.y;
+console.log(" leftwristX= " + leftwristX + " leftwristY= " + leftwristY)
+
+rightwristX=results[0].pose.rightWrist.x;
+rightwristY=results[0].pose.rightWrist.y;
+console.log(" rightwristX= " + rightwristX + " rightwristY= " + rightwristY)
+}
 }

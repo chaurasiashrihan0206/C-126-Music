a9cf54f7a6261aa52681e8050f826cde6ef9d3c0
Shrihan Chaurasia <78851287+chaurasiashrihan0206@users.noreply.github.com>
2021-11-26 15:56:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ leftwristX=0;
 leftwristY=0;
 rightwristX=0;
 rightwristY=0;
+
+scoreLW= 0;
+scoreRW= 0;
 function preload(){
     song= loadSound("music.mp3")
 }
@@ -22,6 +25,40 @@ function setup(){
 
 function draw(){
     image(video , 0 , 0 , 400 , 400);
+    fill("#FF0000");
+    stroke("#FF0000");
+    if(scoreRW>0.2){
+        circle(rightwristX , rightwristY , 20);
+        if(rightwristY>0 && rightwristY<=100){
+            document.getElementById("speed").innerHTML= "speed= 0.5x";
+            song.rate(0.5);
+        }
+        else if(rightwristY>100 && rightwristY<=200){
+            document.getElementById("speed").innerHTML= "speed= 1x";
+            song.rate(1);
+        }
+        else if(rightwristY>200 && rightwristY<=300){
+            document.getElementById("speed").innerHTML= "speed= 1.5x";
+            song.rate(1.5);
+        }
+        else if(rightwristY>300 && rightwristY<=400){
+            document.getElementById("speed").innerHTML= "speed= 2x";
+            song.rate(2);
+        }
+        else if(rightwristY>400 && rightwristY<=500){
+            document.getElementById("speed").innerHTML= "speed= 2.5x";
+            song.rate(2.5);
+        }
+    }
+    
+    if(scoreLW>0.2){
+        circle(leftwristX , leftwristY , 20);
+        a= Number(leftwristY);
+        b= floor(a);
+        volume= b/500;
+        document.getElementById("volume").innerHTML= "volume= " + volume;
+        song.setVolume(volume);
+    }
 }
 
 function play(){
@@ -37,6 +74,8 @@ function modelLoaded(){
 function gotPoses(results){
 if(results.length>0){
     console.log(results);
+    scoreRW= results[0].pose.keypoints[10].score;
+    scoreLW= results[0].pose.keypoints[9].score;
 leftwristX=results[0].pose.leftWrist.x;
 leftwristY=results[0].pose.leftWrist.y;
 console.log(" leftwristX= " + leftwristX + " leftwristY= " + leftwristY)
